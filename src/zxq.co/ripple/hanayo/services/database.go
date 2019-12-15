81cce19b93b0411b4ec12d0860d4bed14393f52a
Josh <cmyuisuki@gmail.com>
2019-12-15 02:58:38 -0500

    lmao src

--- src/zxq.co/ripple/hanayo/services/database.go
+++
@@ -1,6 +0,0 @@
-package services
-
-// A Database should provide a reliable datastore in which to store the data
-// required for Hanayo to run.
-type Database interface {
-}
--- src/zxq.co/ripple/hanayo/static/key_plural.js
+++
@@ -1,1 +0,0 @@
-var keyPlurals = {"{{ stars }} star":"{{ stars }} stars","<b>{{ months }}</b> month costs <b>€ {{ eur }}</b>":"<b>{{ months }}</b> months cost <b>€ {{ eur }}</b>"};
