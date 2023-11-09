3485ec8b3a897c52a976b7a1910fce9b31760564
metal-face <hughesbryan3000@gmail.com>
2023-11-09 00:13:46 -0500

    ioutil is deprecated, it calls io

--- app/handlers/profiles/settings/userpage.go
+++ app/handlers/profiles/settings/userpage.go
@@ -1,14 +1,14 @@
 package settings
 
 import (
-	"io/ioutil"
+	"io"
 
 	"github.com/gin-gonic/gin"
 	"github.com/osuAkatsuki/hanayo/internal/bbcode"
 )
 
 func ParseBBCodeSubmitHandler(c *gin.Context) {
-	body, err := ioutil.ReadAll(c.Request.Body)
+	body, err := io.ReadAll(c.Request.Body)
 	if err != nil {
 		c.Error(err)
 		c.String(200, "Error")
