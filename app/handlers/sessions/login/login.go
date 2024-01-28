76a571fb817fadb3a0e501a57b253d483a7a3dd4
tsunyoku <tsunyoku@gmail.com>
2024-01-28 00:04:31 +0000

    move reused logic

--- app/handlers/sessions/login/login.go
+++ app/handlers/sessions/login/login.go
@@ -11,6 +11,7 @@ import (
 	"golang.org/x/exp/slog"
 
 	"github.com/amplitude/analytics-go/amplitude"
+	s "github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/mileusna/useragent"
 	"github.com/osuAkatsuki/akatsuki-api/common"
@@ -28,6 +29,17 @@ import (
 	"github.com/osuAkatsuki/otp-service-client-go/client"
 	"golang.org/x/crypto/bcrypt"
 )
+
+type SessionData struct {
+	ID              int
+	Username        string
+	Password        string
+	PasswordVersion int
+	Country         string
+	pRaw            int64
+	Privileges      common.UserPrivileges
+	Flags           uint
+}
 
 func LoginSubmitHandler(c *gin.Context) {
 	if sessions.GetContext(c).User.ID != 0 {
@@ -48,16 +60,7 @@ func LoginSubmitHandler(c *gin.Context) {
 		u = uu.SafeUsername(u)
 	}
 
-	var data struct {
-		ID              int
-		Username        string
-		Password        string
-		PasswordVersion int
-		Country         string
-		pRaw            int64
-		Privileges      common.UserPrivileges
-		Flags           uint
-	}
+	var data SessionData
 	err := services.DB.QueryRow(`
 	SELECT
 		u.id, u.password_md5,
@@ -159,73 +162,7 @@ func LoginSubmitHandler(c *gin.Context) {
 		return
 	}
 
-	latitude, err := strconv.ParseFloat(c.Request.Header.Get("CF-IPLatitude"), 64)
-	if err != nil {
-		slog.Error("Error parsing latitude", "error", err.Error())
-		latitude = 0.0
-	}
-
-	longitude, err := strconv.ParseFloat(c.Request.Header.Get("CF-IPLongitude"), 64)
-	if err != nil {
-		slog.Error("Error parsing longitude", "error", err.Error())
-		longitude = 0.0
-	}
-
-	userAgent := useragent.Parse(c.Request.UserAgent())
-
-	services.Amplitude.Track(amplitude.Event{
-		EventType: "web_login",
-		EventOptions: amplitude.EventOptions{
-			UserID:      strconv.Itoa(data.ID),
-			IP:          c.ClientIP(),
-			Country:     c.Request.Header.Get("CF-IPCountry"),
-			City:        c.Request.Header.Get("CF-IPCity"),
-			LocationLat: latitude,
-			LocationLng: longitude,
-			Region:      c.Request.Header.Get("CF-Region"),
-			Language:    c.Request.Header.Get("Accept-Language"),
-			OSName:      userAgent.OS,
-			OSVersion:   userAgent.OSVersion,
-			DeviceModel: userAgent.Device,
-		},
-		EventProperties: map[string]interface{}{"source": "hanayo"},
-	})
-
-	identifyObj := amplitude.Identify{}
-	identifyObj.Set("last_login", time.Now().Unix())
-	identifyObj.Set("last_login_country", data.Country)
-	identifyObj.Set("last_login_ip", c.ClientIP())
-	identifyObj.Set("os_name", userAgent.OS)
-	identifyObj.Set("os_version", userAgent.OSVersion)
-	identifyObj.Set("device_model", userAgent.Device)
-
-	services.Amplitude.Identify(
-		identifyObj,
-		amplitude.EventOptions{
-			UserID: strconv.Itoa(data.ID),
-		},
-	)
-
-	uu.SetYCookie(data.ID, c)
-
-	sess.Set("userid", data.ID)
-	sess.Set("pw", cryptography.MakeMD5(data.Password))
-	sess.Set("logout", common.RandomString(15))
-
-	AfterLogin(c, data.ID, data.Country, data.Flags)
-
-	redir := c.PostForm("redir")
-	if len(redir) > 0 && redir[0] != '/' {
-		redir = ""
-	}
-
-	sessions.AddMessage(c, msg.SuccessMessage{lu.T(c, "Hey %s! You are now logged in.", template.HTMLEscapeString(data.Username))})
-	sess.Save()
-	if redir == "" {
-		redir = "/"
-	}
-	c.Redirect(302, redir)
-	return
+	handleSession(c, sess, data)
 }
 
 func AfterLogin(c *gin.Context, id int, country string, flags uint) {
@@ -291,15 +228,7 @@ func LoginOtpSubmitHandler(c *gin.Context) {
 	sess.Delete("user_id")
 	sess.Delete("mustotp")
 
-	var data struct {
-		ID         int
-		Username   string
-		Password   string
-		Country    string
-		pRaw       int64
-		Privileges common.UserPrivileges
-		Flags      uint
-	}
+	var data SessionData
 	err = services.DB.QueryRow(`
 	SELECT
 		u.id, u.username, u.password_md5,
@@ -319,6 +248,14 @@ func LoginOtpSubmitHandler(c *gin.Context) {
 		return
 	}
 
+	if rememberDevice {
+		uu.SetRememberedDeviceCookie(data.ID, c)
+	}
+
+	handleSession(c, sess, data)
+}
+
+func handleSession(c *gin.Context, sess s.Session, data SessionData) {
 	latitude, err := strconv.ParseFloat(c.Request.Header.Get("CF-IPLatitude"), 64)
 	if err != nil {
 		slog.Error("Error parsing latitude", "error", err.Error())
@@ -371,10 +308,6 @@ func LoginOtpSubmitHandler(c *gin.Context) {
 	sess.Set("userid", data.ID)
 	sess.Set("pw", cryptography.MakeMD5(data.Password))
 	sess.Set("logout", common.RandomString(15))
-
-	if rememberDevice {
-		uu.SetRememberedDeviceCookie(data.ID, c)
-	}
 
 	AfterLogin(c, data.ID, data.Country, data.Flags)
 
