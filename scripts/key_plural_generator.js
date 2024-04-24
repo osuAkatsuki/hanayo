a28313da63e46d5b3d5f9aa032243489910fa97e
lenforiee <lenforiee@gmail.com>
2024-04-24 19:33:00 +0200

    fix generate plural script

--- scripts/key_plural_generator.js
+++ scripts/key_plural_generator.js
@@ -1,25 +1,29 @@
-var parser = require("gettext-parser");
-var fs = require("fs");
+import { po } from "gettext-parser";
+import { readFileSync, writeFileSync } from "fs";
 
-fs.readFile("locale/js-locales/templates.po.example", "utf-8", (err, data) => {
-  if (err) throw err;
-  var tpl = parser.po.parse(data, "utf-8");
-  var plurals = {};
-  Object.keys(tpl.translations[""]).forEach((key) => {
-    let val = tpl.translations[""][key];
+readFileSync(
+  "../locale/js-locales/templates.po.example",
+  "utf-8",
+  (err, data) => {
+    if (err) throw err;
+    var tpl = po.parse(data, "utf-8");
+    var plurals = {};
+    Object.keys(tpl.translations[""]).forEach((key) => {
+      let val = tpl.translations[""][key];
 
-    if (typeof val === "undefined") return;
+      if (typeof val === "undefined") return;
 
-    if (!val.msgid_plural) return;
+      if (!val.msgid_plural) return;
 
-    plurals[val.msgid] = val.msgid_plural;
-  });
+      plurals[val.msgid] = val.msgid_plural;
+    });
 
-  fs.writeFile(
-    "web/src/js/key_plural.js",
-    "var keyPlurals = " + JSON.stringify(plurals) + ";",
-    (err) => {
-      if (err) throw err;
-    }
-  );
-});
+    writeFileSync(
+      "../web/src/js/key_plural.js",
+      "var keyPlurals = " + JSON.stringify(plurals) + ";",
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+);
