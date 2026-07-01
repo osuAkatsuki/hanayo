5bf447d87deea93cb573e2d538710cb0fa2d5253
Josh Smith <cmyuiosu@gmail.com>
2026-07-01 01:59:19 -0400

    Add OAuth state to connection link flows (#265)
    * Add OAuth state to connection links
    
    * Use temporary redirects for OAuth links

--- app/handlers/connections/discord.go
+++ app/handlers/connections/discord.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"net/url"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	eh "github.com/osuAkatsuki/hanayo/app/handlers/errors"
 	"github.com/osuAkatsuki/hanayo/app/sessions"
 	tu "github.com/osuAkatsuki/hanayo/app/usecases/templates"
 
 	lu "github.com/osuAkatsuki/hanayo/app/usecases/localisation"
+	"github.com/osuAkatsuki/hanayo/app/usecases/oauthstate"
 
 	msg "github.com/osuAkatsuki/hanayo/app/models/messages"
 
@@ -34,11 +37,26 @@ func LinkDiscordHandler(c *gin.Context) {
 	}
 
 	settings := settingsState.GetSettings()
+	state, err := oauthstate.New(
+		"discord",
+		int(sessions.GetContext(c).User.ID),
+		settings.APP_HANAYO_KEY,
+		time.Now().Add(10*time.Minute),
+	)
+	if err != nil {
+		eh.Resp500(c)
+		return
+	}
 
 	discordCallbackUrl := fmt.Sprintf("%s/discord/callback", settings.PUBLIC_AKATSUKI_API_BASE_URL)
-	redirectUrl := fmt.Sprintf("https://discord.com/oauth2/authorize?client_id=%s&redirect_uri=%s&response_type=code&scope=identify", settings.DISCORD_CLIENT_ID, discordCallbackUrl)
+	query := url.Values{}
+	query.Set("client_id", settings.DISCORD_CLIENT_ID)
+	query.Set("redirect_uri", discordCallbackUrl)
+	query.Set("response_type", "code")
+	query.Set("scope", "identify")
+	query.Set("state", state)
 
-	c.Redirect(301, redirectUrl)
+	c.Redirect(302, "https://discord.com/oauth2/authorize?"+query.Encode())
 }
 
 func LinkTwitchHandler(c *gin.Context) {
@@ -57,11 +75,23 @@ func LinkTwitchHandler(c *gin.Context) {
 	}
 
 	settings := settingsState.GetSettings()
+	state, err := oauthstate.New(
+		"twitch",
+		int(sessions.GetContext(c).User.ID),
+		settings.APP_HANAYO_KEY,
+		time.Now().Add(10*time.Minute),
+	)
+	if err != nil {
+		eh.Resp500(c)
+		return
+	}
+
 	twitchCallbackUrl := fmt.Sprintf("%s/twitch/callback", settings.PUBLIC_AKATSUKI_API_BASE_URL)
 	query := url.Values{}
 	query.Set("client_id", settings.TWITCH_CLIENT_ID)
 	query.Set("redirect_uri", twitchCallbackUrl)
 	query.Set("response_type", "code")
+	query.Set("state", state)
 
-	c.Redirect(301, "https://id.twitch.tv/oauth2/authorize?"+query.Encode())
+	c.Redirect(302, "https://id.twitch.tv/oauth2/authorize?"+query.Encode())
 }
